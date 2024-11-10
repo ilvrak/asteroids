b83d8b80ff575b458f2639a4c0afe38ccacb7c8f
ilvrak <ilvrak@gmail.com>
2024-11-11 00:37:02 +0800

    ch2 4

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import pygame
 from constants import *
 
 
-screen = pygame.display.set_mode(SCREEN_RES)
+def main():
+    pygame.init
+    screen = pygame.display.set_mode(SCREEN_RES)
+    clock = pygame.time.Clock()
+    dt = 0
 
-while True:
-    screen.fill('black')
-    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        return
+        screen.fill('black')
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == '__main__':
