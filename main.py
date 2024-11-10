cab7d96c9f1c42b257962de88d70bb6e30c48b58
ilvrak <ilvrak@gmail.com>
2024-11-11 00:26:19 +0800

    ch2 2

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pygame
 from constants import *
 
 
-def main():
-    print('Starting asteroids!')
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+screen = pygame.display.set_mode(SCREEN_RES)
+
+while True:
+    screen.fill('black')
+    pygame.display.flip()
+
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        return
 
 
 if __name__ == '__main__':
